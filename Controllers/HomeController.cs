d1e5940a9c2a4613be25cf616c2400cedfa122a8
Vardo Ternos <tvardero@gmail.com>
2022-03-25 20:19:48 +0200

    Remove unused fields from HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Groceries.Controllers;
 
 public class HomeController : Controller
 {
-    public HomeController(IRepository<Product> productRepository, IRepository<Category> categoryRepository)
-    {
-        _productRepository = productRepository;
-        _categoryRepository = categoryRepository;
-    }
-
     public IActionResult Index()
     {
         return View();
     }
-
-    private readonly IRepository<Product> _productRepository;
-    private readonly IRepository<Category> _categoryRepository;
 }
