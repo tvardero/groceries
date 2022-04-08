21be3332a5707bb106fc853d6c3319cd007145ac
Vardo Ternos <tvardero@gmail.com>
2022-04-08 22:21:06 +0300

    Add Cart model, controller and related views

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -34,10 +34,63 @@ public class CartController : Controller
             return RedirectToAction(nameof(Index), new { returnUrl });
         }
 
-        TempData["Message"] = "Something went wrong, try again.";
+        TempData["Message"] = "Щось пішло не так, спробуйте ще раз.";
         TempData["MessageColor"] = "danger";
-        return Redirect("/");
+        return Redirect(returnUrl ?? "/");
     }
+
+    public IActionResult Update(AddToCartViewModel model, string? returnUrl)
+    {
+        if (ModelState.IsValid)
+        {
+            CartItem item = _cart.Items.Single(ci => ci.Product.Id == model.ProductId);
+            item.Quantity = model.Quantity;
+            return RedirectToAction(nameof(Index), new { returnUrl });
+        }
+
+        TempData["Message"] = "Щось пішло не так, спробуйте ще раз.";
+        TempData["MessageColor"] = "danger";
+        return RedirectToAction(nameof(Index), new { returnUrl });
+    }
+
+    public IActionResult Delete(AddToCartViewModel model, string? returnUrl)
+    {
+        if (ModelState.IsValid)
+        {
+            CartItem item = _cart.Items.Single(ci => ci.Product.Id == model.ProductId);
+            _cart.Items.Remove(item);
+            return RedirectToAction(nameof(Index), new { returnUrl });
+        }
+
+        TempData["Message"] = "Щось пішло не так, спробуйте ще раз.";
+        TempData["MessageColor"] = "danger";
+        return RedirectToAction(nameof(Index), new { returnUrl });
+    }
+
+    public IActionResult Checkout(string? returnUrl = null)
+    {
+        if (_cart.TotalPrice > 0)
+        {
+            return View(new CheckoutViewModel() { ReturnUrl = returnUrl });
+        }
+
+        TempData["Message"] = "Сума покупки повинна бути більшою за нуль.";
+        TempData["MessageColor"] = "danger";
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public IActionResult Checkout(CheckoutViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            _cart.Items.Clear();
+            return RedirectToAction(nameof(Success));
+        }
+        return View(model);
+    }
+
+    public IActionResult Success() => View();
 
     private readonly Cart _cart;
     private readonly IRepository<Product> _productRepository;
---
+++ Models/ViewModels/CheckoutViewmodel.cs
@@ -0,0 +1,53 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Groceries.Models.ViewModels;
+
+public class CheckoutViewModel
+{
+    [Required(ErrorMessage = "Потрібно вказати ім'я"), MaxLength(50)]
+    [Display(Name = "Ім'я")]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Потрібно вказати призвище"), MaxLength(50)]
+    [Display(Name = "Призвище")]
+    public string LastName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Потрібно вказати номер телефону"), MaxLength(9)]
+    [Display(Name = "Номер телефону")]
+    [DataType(DataType.PhoneNumber)]
+    public string Phone { get; set; } = null!;
+
+    [Required(ErrorMessage = "Потрібно вказати e-mail"), MaxLength(100)]
+    [Display(Name = "E-mail")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Потрібно вказати область"), MaxLength(50)]
+    [Display(Name = "Область")]
+    public string AddressRegion { get; set; } = null!;
+
+    [Required(ErrorMessage = "Потрібно вказати населений пункт"), MaxLength(50)]
+    [Display(Name = "Населений пункт")]
+    public string AddressCity { get; set; } = null!;
+
+    [Required(ErrorMessage = "Потрібно вказати вулицю"), MaxLength(50)]
+    [Display(Name = "Вулиця")]
+    public string AddressStreet { get; set; } = null!;
+
+    [Required(ErrorMessage = "Потрібно вказати номер дому"), MaxLength(50)]
+    [Display(Name = "Номер дому")]
+    public string AddressHouse { get; set; } = null!;
+
+    [MaxLength(50)]
+    [Display(Name = "Квартира")]
+    public string? AddressFlat { get; set; }
+
+    [MaxLength(50)]
+    [Display(Name = "Поштовий індекс")]
+    [DataType(DataType.PostalCode)]
+    public string? AddressPostalCode { get; set; }
+
+    [HiddenInput]
+    public string? ReturnUrl { get; set; }
+}
