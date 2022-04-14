fdb6fa72574ac19c02fd8cc5b5968c4d12c8f67c
Vardo Ternos <tvardero@gmail.com>
2022-04-14 23:08:51 +0300

    Add route for categories

--- Program.cs
+++ Program.cs
@@ -33,6 +33,12 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: null!,
+    pattern: "{categoryId=0}",
+    defaults: new { controller = "Products", action = "Index" }
+);
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Products}/{action=Index}"
 );
